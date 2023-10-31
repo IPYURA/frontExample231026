c3484556a6cbf0b24d05cd3c305bad33d753f69c
IPYURA <0207ehdrjs@naver.com>
2023-10-31 11:51:44 +0900

    두 번째 업로드

---
+++ uppercase/js.js
@@ -0,0 +1,20 @@
+// let text = prompt("소문자 문자열 입력 :");
+// let textarr = text.split("");
+// console.log(textarr);
+// textarr[0].toUpperCase();
+// function upperText(arr) {
+//     let firstLetter = arr[0].toUpperCase();
+//     arr.splice(0,1,firstLetter);
+//     return arr;
+// }
+// const area = document.querySelector("#textarea");
+// area.innerText = upperText(textarr).join("");
+
+//수업1
+// const area = document.querySelector("#textarea");
+// const string = prompt("영문 소문자로 된 문자열 입력 : ");
+// const firstCh = string[0].toUpperCase();
+// const remainCh = string.slice(1);
+// const result = firstCh + remainCh;
+// const result01 = [string[0].toUpperCase(), ...string.slice(1)].join("");
+// area.innerText = result01;
